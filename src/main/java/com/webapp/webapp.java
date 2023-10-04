2a6c9d68058690702b4f33eaa0c7ce9956c19bda
NavreetK <46690891+NavreetK@users.noreply.github.com>
2023-10-04 13:37:24 -0400

    mapping added

--- src/main/java/com/webapp/webapp.java
+++ src/main/java/com/webapp/webapp.java
@@ -1,5 +1,11 @@
 package com.webapp;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("docker-app")
 public class webapp 
 {
     public static void main( String[] args )
