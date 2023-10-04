b2199508aae2bb6e52da8780bcfab73ee0b95d8a
NavreetK <46690891+NavreetK@users.noreply.github.com>
2023-10-04 14:46:55 -0400

    Update Webapp.java

--- src/main/java/com/webapp/Webapp.java
+++ src/main/java/com/webapp/Webapp.java
@@ -1,1 +1,14 @@
 
+package com.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webapp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webapp.class, args);
+	}
+
+}
